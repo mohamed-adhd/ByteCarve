c884925dff95537eba49232176883dd95f3083d6
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-06 22:16:49 +0000

    pick window styled and filedialogue added

--- ByteCarve/ViewModels/PickViewModel.cs
+++ ByteCarve/ViewModels/PickViewModel.cs
@@ -1,6 +1,24 @@
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Linq;
+using System.Threading.Tasks;
 namespace ByteCarve.ViewModels;
-
-public class PickViewModel :ViewModelBase
+public partial class PickViewModel : ViewModelBase
 {
-    
+    [ObservableProperty] private string? selectedFilePath;
+    public IStorageProvider? StorageProvider { get; set; }
+    [RelayCommand]
+    private async Task ChooseFile()
+    {
+        if (StorageProvider is null) return;
+        var files = await StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "select a file twin",
+            AllowMultiple = false
+        });
+        var file = files.FirstOrDefault();
+        if (file is not null)
+            SelectedFilePath = file.Path.LocalPath;
+    }
 }
--- ByteCarve/Views/PickView.axaml.cs
+++ ByteCarve/Views/PickView.axaml.cs
@@ -1,10 +1,16 @@
 using Avalonia.Controls;
-
+using ByteCarve.ViewModels;
 namespace ByteCarve.Views;
 public partial class PickView : UserControl
 {
     public PickView()
     {
         InitializeComponent();
+        Loaded += (_, _) =>
+        {
+            var topLevel = TopLevel.GetTopLevel(this);
+            if (DataContext is PickViewModel vm && topLevel is not null)
+                vm.StorageProvider = topLevel.StorageProvider;
+        };
     }
 }
