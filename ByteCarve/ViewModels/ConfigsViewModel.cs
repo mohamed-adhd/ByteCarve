1330a42787599d5a59ee5c1413595a94b02ed363
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-09 01:08:17 +0000

    config page viewmodel has been built , name, result folder picker , carve type

--- ByteCarve/ViewModels/ConfigsViewModel.cs
+++ ByteCarve/ViewModels/ConfigsViewModel.cs
@@ -1,8 +1,41 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Linq;
+using Avalonia.Platform.Storage;
+using System.Threading.Tasks;
 
 namespace ByteCarve.ViewModels;
 
-public class ConfigsViewModel
+public partial class ConfigsViewModel : ViewModelBase
 {
-    [ObservableProperty] private string mode;
+    [ObservableProperty] public string mode;
+    [ObservableProperty] public bool isMediaMode = false;
+    [ObservableProperty] public bool isCodeMode = false;
+    [ObservableProperty] public string oP;
+    public IStorageProvider? StorageProvider { get; set; }
+
+    [RelayCommand]
+    public void SelectMediaMode()
+    {
+        IsMediaMode = true;
+    }
+    [RelayCommand]
+    public void SelectCodeMode()
+    {
+        IsCodeMode = true;
+    }
+    [RelayCommand]
+    public async Task PickOutputFolder()
+    {
+        if (StorageProvider is null) return;
+        var files = await StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "select a file twin",
+            AllowMultiple = false
+        });
+        var file = files.FirstOrDefault();
+        if (file is not null)
+            OP = file.Path.LocalPath;
+        
+    }
 }
