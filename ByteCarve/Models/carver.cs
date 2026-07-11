69d7b0eaddbf53559b7e23ca54380d21e305aa68
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-11 10:09:58 +0000

    chunks length detector added using bigEndian

--- ByteCarve/Models/carver.cs
+++ ByteCarve/Models/carver.cs
@@ -1,13 +1,16 @@
 using System;
+using System.Buffers.Binary;
 using static System.Text.Encoding;
+using Tmds.DBus.Protocol;
 using System.IO;
 using System.Collections.Generic;
 namespace ByteCarve.Models;
 using static  System.IO.File;
-
+using System.Linq;
 public class carver
 
 {
+    public List<byte[]> images = new();    
     public int pngStart,pngEnd;
     private int index = 0;
     private byte[] file;
@@ -22,35 +25,33 @@ public class carver
     }
 
     public void Carvethashi()
+    
     {
-      cur8=file.AsSpan(index, 8).ToArray();
+      
         while (type != "IEND")
         {
+            cur8=file.AsSpan(index, 8).ToArray();
             if (cur8.SequenceEqual(PngSig))
             {
                 pngStart = index+8;
                 while (type != "IEND")
                 {
-                    byte[] temp=file.AsSpan(pngStart+8, 8).ToArray();
-                    int len = int.Parse(ASCII.GetString(file, pngStart, 4));
-                    string type = (ASCII.GetString(file, pngStart + 4, 4));
+                    uint len = BinaryPrimitives.ReadUInt32BigEndian(file.AsSpan(pngStart + 8, 4));
+                    type = (ASCII.GetString(file, pngStart + 4, 4));
                     if (type == "IHDR")
                     {
+                        images.Add(file.AsSpan(pngStart+16, (int)len).ToArray());
                         
                     }
-
-
-
-
                 }
+                
             }
-            
+
+            index++;
+
         }
         
     }
 
-    public void parser(int s)
-    {
-        int cp=pngStart+
-    }
+    
 }
