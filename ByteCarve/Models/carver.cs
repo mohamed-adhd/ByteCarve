4182f314a3771dcdf0642b0e3f512a64cfc1707e
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-11 10:41:08 +0000

    okay so the chunk reader is done , now just adding the end boundaries, and will test it later

--- ByteCarve/Models/carver.cs
+++ ByteCarve/Models/carver.cs
@@ -17,6 +17,7 @@ public class carver
     int value = 0;
     private static readonly byte[] PngSig={0x89,0x50,0x4E,0x47,0x0D,0x0A,0x1A,0x0A};
     byte[] cur8; 
+    Boolean looking = true;
     string type = "";
     public carver(string path)
     {
@@ -28,20 +29,28 @@ public class carver
     
     {
       
-        while (type != "IEND")
+        while (index <= file.Length - PngSig.Length)
         {
             cur8=file.AsSpan(index, 8).ToArray();
             if (cur8.SequenceEqual(PngSig))
             {
-                pngStart = index+8;
-                while (type != "IEND")
+                pngStart = index;
+                int cp= pngStart + 8;
+                while (looking)
                 {
-                    uint len = BinaryPrimitives.ReadUInt32BigEndian(file.AsSpan(pngStart + 8, 4));
-                    type = (ASCII.GetString(file, pngStart + 4, 4));
-                    if (type == "IHDR")
+                    if (cp + 12 > file.Length)
+                        break;
+                    uint len = BinaryPrimitives.ReadUInt32BigEndian(file.AsSpan(cp, 4));
+                    type = (ASCII.GetString(file, cp+4, 4));
+                    if (type !="IEND")
                     {
-                        images.Add(file.AsSpan(pngStart+16, (int)len).ToArray());
-                        
+                        cp+= 12 + (int)len;
+                    }
+                    else
+                    {
+                        pngEnd=index+20 + (int)len;
+                        looking = true;
+                        images.Add(file.AsSpan(pngStart, pngEnd-pngStart).ToArray());
                     }
                 }
                 
