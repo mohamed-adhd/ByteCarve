4c83845947698de9fddcdbf2e8b8e4e247f7c33a
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-14 21:45:02 +0000

    end to end tested fully functional , starting disassembler

--- ByteCarve/ViewModels/HistoryViewModel.cs
+++ ByteCarve/ViewModels/HistoryViewModel.cs
@@ -7,11 +7,13 @@ using System.Collections.Generic;
 public partial class HistoryViewModel: ViewModelBase
 {
     private ByteCarve.ViewModels.MainWindowViewModel _main;
+    private database sql=new database();
     [ObservableProperty] private List<database.ops> history=null;
 
     public HistoryViewModel(MainWindowViewModel s)
     {
         _main = s;
+        History = sql.fetch();
     }
     [RelayCommand]
     public void Back2m()
