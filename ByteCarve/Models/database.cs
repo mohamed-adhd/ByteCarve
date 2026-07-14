8de591616529fa9f94c8fe3e627f616e0ef01a68
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-14 20:03:18 +0000

    add method implemented

--- ByteCarve/Models/database.cs
+++ ByteCarve/Models/database.cs
@@ -4,6 +4,25 @@ using static System.Console;
 
 public class database
 {
-    private string path=""
+    private string path="/home/bro/my-creations/ByteCarve/ByteCarve/Assets/admin.db";
+    public SqliteConnection con;
+    public database()
+    {
+        con= new SqliteConnection(path);
+        
+    }
+
+    public void add(string n,string a,string t,int i,int d)
+    {
+        con.Open();
+        using var cmd = con.CreateCommand();
+        cmd.CommandText = "INSERT INTO tests (test_name,app_name,type,images,duration) VALUES ($n,$a,$t,$i,$d);";
+        cmd.Parameters.AddWithValue("$n", n);
+        cmd.Parameters.AddWithValue("$a", a);
+        cmd.Parameters.AddWithValue("$t", t);
+        cmd.Parameters.AddWithValue("$i", i);
+        cmd.Parameters.AddWithValue("$d", d);
+        cmd.ExecuteNonQuery();
+    }
     
 }
