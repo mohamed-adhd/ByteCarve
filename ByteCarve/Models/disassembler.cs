e8b333e776c85bede001db6d176a8d6198bf0ef3
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-14 22:54:20 +0000

    25-28 signal extractor is added

--- ByteCarve/Models/disassembler.cs
+++ ByteCarve/Models/disassembler.cs
@@ -1,15 +1,53 @@
+using System.Collections;
+using System;
+using System.Buffers.Binary;
+using System.Linq;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
-
+using static  System.IO.File;
 namespace ByteCarve.Models;
 
 public class disassembler
 {
-    public string path;
+    public byte[] data,chunk;
+    private BitArray bitchunk;
+    private bool looking = true;
+    public int index = 0;
 
     public disassembler(string path)
     {
-        this.path = path;
+        data=ReadAllBytes(path);
     }
+
+    public void lord_have_mercy()
+    {
+        while (looking)
+        {
+            chunk = data.AsSpan(index,4).ToArray();
+            BitArray result = new BitArray(new BitArray(chunk).Cast<bool>().Skip(25).Take(4).ToArray());
+            byte[] bytes = new byte[(result.Length + 7)/8];// extracting a byte section , taking a bits chunk from it and then transforming it back into bytes  , yay i m having so much fun on 10:52 on a random fucking tuesday night
+            result.CopyTo(bytes, 0);
+            int sig = BinaryPrimitives.ReadInt32LittleEndian(bytes);
+            
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     public void DP_immediate(byte[] data)
     {
