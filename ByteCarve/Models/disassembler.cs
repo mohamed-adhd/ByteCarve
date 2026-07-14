be27a630268255149ff183fd500e7d1b6351ccfd
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-14 22:06:21 +0000

    jokes aside ,i may have opened the gates of hell. 5 buckets system for decoding and AArch64 is the way i will be taking in this section.

--- ByteCarve/Models/disassembler.cs
+++ ByteCarve/Models/disassembler.cs
@@ -10,4 +10,25 @@ public class disassembler
     {
         this.path = path;
     }
+
+    public void DP_immediate(byte[] data)
+    {
+        
+    }
+    public void Branches(byte[] data)
+    {
+        
+    }
+    public void Loads(byte[] data)
+    {
+        
+    }
+    public void DP_Register(byte[] data)
+    {
+        
+    }
+    public void DP_Scalar(byte[] data)
+    {
+        
+    }
 }
