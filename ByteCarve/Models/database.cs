1884b1b97b761ffa169d0abf8834a18c2dfba642
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-14 20:11:44 +0000

    fetch method added

--- ByteCarve/Models/database.cs
+++ ByteCarve/Models/database.cs
@@ -1,9 +1,19 @@
 namespace ByteCarve.Models;
 using Microsoft.Data.Sqlite;
 using static System.Console;
+using System.Collections.Generic;
 
 public class database
 {
+    public class ops
+    {
+        public string name;
+        public string a_name;
+        public string type;
+        public int images;
+        public int dur;
+
+    }
     private string path="/home/bro/my-creations/ByteCarve/ByteCarve/Assets/admin.db";
     public SqliteConnection con;
     public database()
@@ -23,6 +33,27 @@ public class database
         cmd.Parameters.AddWithValue("$i", i);
         cmd.Parameters.AddWithValue("$d", d);
         cmd.ExecuteNonQuery();
+    }
+
+    public List<ops> fetch()
+    {
+        List<ops> tempo=new List<ops>();
+        con.Open();
+        using var cmd = con.CreateCommand();
+        cmd.CommandText = "SELECT * FROM tests;";
+        using var res = cmd.ExecuteReader();
+        while (res.Read())
+        {
+            ops temp=new ops();
+            temp.name = res.GetString(0);
+            temp.a_name = res.GetString(1);
+            temp.type = res.GetString(2);
+            temp.images = res.GetInt32(3);
+            temp.dur = res.GetInt32(4);
+            tempo.Add(temp);
+        }
+
+        return tempo;
     }
     
 }
