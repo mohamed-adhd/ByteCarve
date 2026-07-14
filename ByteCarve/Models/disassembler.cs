478d0b4db531a5c0d65ad318244ab1616cd71477
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-14 23:07:20 +0000

    littleEndian32 was overkill , switched to a simple 1/0 counter

--- ByteCarve/Models/disassembler.cs
+++ ByteCarve/Models/disassembler.cs
@@ -25,9 +25,14 @@ public class disassembler
         {
             chunk = data.AsSpan(index,4).ToArray();
             BitArray result = new BitArray(new BitArray(chunk).Cast<bool>().Skip(25).Take(4).ToArray());
-            byte[] bytes = new byte[(result.Length + 7)/8];// extracting a byte section , taking a bits chunk from it and then transforming it back into bytes  , yay i m having so much fun on 10:52 on a random fucking tuesday night
+            byte[] bytes = new byte[4];// extracting a byte section , taking a bits chunk from it and then transforming it back into bytes  , yay i m having so much fun on 10:52 on a random fucking tuesday night
             result.CopyTo(bytes, 0);
-            int sig = BinaryPrimitives.ReadInt32LittleEndian(bytes);
+            int sig = 0;
+            for (int i = 0; i < 4; i++)
+                if (result[i])
+                {
+                    sig |= (1 << i);
+                }
             
         
         
