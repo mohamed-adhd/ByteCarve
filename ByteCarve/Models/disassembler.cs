d30e8827eee8d20679066115a52a0324ddb2eccf
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-14 20:14:31 +0000

    disassembler class and constrcutor added , will first add the history page to get rid of it of my back

---
+++ ByteCarve/Models/disassembler.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.Design;
+
+namespace ByteCarve.Models;
+
+public class disassembler
+{
+    public string path;
+
+    public disassembler(string path)
+    {
+        this.path = path;
+    }
+}
