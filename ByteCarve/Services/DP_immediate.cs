134db9d2690513bb362e22f2dad8c88bffa17424
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-15 21:18:16 +0000

    pcaddress section is at prototype now

--- ByteCarve/Services/DP_immediate.cs
+++ ByteCarve/Services/DP_immediate.cs
@@ -6,21 +6,22 @@ using System.Collections;
 using System.Linq;
 public class DP_immediate
 {
-    private byte[] data;
+    private uint word;
+    private int index;
     static uint extractBits(uint word, int hi, int lo)
     {
         int width = hi - lo + 1;
         uint mask = (1u << width) - 1;
         return (word >> lo) & mask;
     }
-    public void process_it(byte[] tempo)
+    public void process_it(byte[] tempo,int ts)
     {
-        data = tempo;
-        uint word=BitConverter.ToUInt32(data, 0);
+        index = ts;
+        uint word=BitConverter.ToUInt32(tempo, 0);
         uint sig=extractBits(word,25, 23);
         if (sig == 0b001 || sig == 0b000)
         {
-            pcaddress(data);
+            pcaddress(word);
         }else if (sig == 0b010 || sig == 0b011)
         {
             addsubs(data);
@@ -39,9 +40,17 @@ public class DP_immediate
         }
     }
 
-    public void pcaddress()
+    public void pcaddress(uint word)
     {
-        
+        if (((word >> 31) & 1 )== 1)
+        {
+            uint immlo=extractBits(word, 29, 30);
+            int rgst = (int)extractBits(word, 0, 4);
+            uint immhi=extractBits(word, 5, 23);
+            int dist = (int)((immhi << 2) | immlo);
+            //here i ll add a file.write(index+": adr x"+"rgst+" #"+(index+8)) 
+
+        }
     }
     
 }
