9fc83db5875192c15c678cbb936a709986f5b337
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-15 21:48:36 +0000

    adrp finished as well , both configured to append to the same file

--- ByteCarve/Services/DP_immediate.cs
+++ ByteCarve/Services/DP_immediate.cs
@@ -1,21 +1,23 @@
 using System;
 using Avalonia.Media.TextFormatting;
-
+using System.IO;
 namespace ByteCarve.Services;
 using System.Collections;
 using System.Linq;
 public class DP_immediate
 {
     private uint word;
-    private int index;
+    private ulong index;
+    private string op;
     static uint extractBits(uint word, int hi, int lo)
     {
         int width = lo-hi + 1;
         uint mask = (1u << width) - 1;
         return (word >> lo) & mask;
     }
-    public void process_it(byte[] tempo,int ts)
+    public void process_it(byte[] tempo,ulong ts,string op)
     {
+        this.op = op;
         index = ts;
         uint word=BitConverter.ToUInt32(tempo, 0);
         uint sig=extractBits(word,25, 23);
@@ -52,7 +54,19 @@ public class DP_immediate
             {
                 dist |= unchecked((int)0xFFE00000);
             }
-            //here i ll add a file.write(index+": adr x"+"rgst+" #"+(index+8)) 
+
+            File.AppendAllText(op + "bytecarve.s", index + ": adr x" + rgst + " #" + (index + 8));
+
+        }
+        else if (((word >> 31) & 1 )== 1)
+        {
+            uint immlo=extractBits(word, 29, 30);
+            int rgst = (int)extractBits(word, 0, 4);
+            uint immhi=extractBits(word, 5, 23);
+            int dist = (int)((immhi << 2) | immlo);
+            int scaled_dist = dist * 4096;
+            ulong rad = index & ~0xFFFUL;
+            File.AppendAllText(op + "bytecarve.s", rad + ": adrp x" + rgst + " #" + (rad+(ulong)scaled_dist));
 
         }
     }
