0f09950228615a52236caa6812a92cc259721bfa
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-18 20:10:31 +0000

    yay there are 11 sub-buckets in dp_register:D (i m losing sanity)

--- ByteCarve/Services/DP_register.cs
+++ ByteCarve/Services/DP_register.cs
@@ -1,5 +1,5 @@
 namespace ByteCarve.Services;
-
+using System;
 public class DP_register
 {
     private uint word;
@@ -11,5 +11,16 @@ public class DP_register
         int width = lo - hi + 1;
         uint mask = (1u << width) - 1;
         return (word >> lo) & mask;
+    }
+
+    public void process_it(byte[] data)
+    {
+        uint word = BitConverter.ToUInt32(data, 0);
+        uint op0 = extractBits(word, 30, 30);
+        uint op1 = extractBits(word, 28, 28);
+        uint op2 = extractBits(word, 21, 24);
+        uint op3 = extractBits(word, 10, 15);
+        
+        
     }
 }
