f6812db91c877deb9b0d35846938ef52012dc170
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-20 20:38:14 +0000

    i aint using the switch for subbucket handling , 4 nested switch is not somehting i would risk adding

--- ByteCarve/Services/DP_scalar.cs
+++ ByteCarve/Services/DP_scalar.cs
@@ -19,5 +19,24 @@ public class DP_scalar
     public void process_it(byte[] data)
     {
         uint word = BitConverter.ToUInt32(data, 0);
+        uint op0 = extractBits(word, 25, 28);
+        uint op1 = extractBits(word, 24, 24);
+        uint op2 = extractBits(word, 21, 21);
+        uint op3 = extractBits(word, 10, 15);
+        switch (op0)
+        {
+            case 0b1111:
+                switch (op1)
+                {
+                    case 1:
+                        fpdp3(word);
+                        break;
+                    case 0:
+                        break;
+                }
+                break;
+            case 0b0111:
+                break;
+        }
     }
 }
