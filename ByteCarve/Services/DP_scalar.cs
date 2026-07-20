d3a6ff4c6680856ee3a05ca818b965fa4ec1e7da
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-20 20:24:46 +0000

    miscalculated sub-buckets count , turns out the scalar bucket consists of +30 sub-buckets , filtered them to 6 (avoided the nerd stats :crypto ,advsimd etc) , same goes for load , its initially 20 but i filtered to 5

--- ByteCarve/Services/DP_scalar.cs
+++ ByteCarve/Services/DP_scalar.cs
@@ -1,5 +1,5 @@
 namespace ByteCarve.Services;
-
+using System;
 public class DP_scalar
 {
     private string op;
@@ -8,5 +8,16 @@ public class DP_scalar
     {
         index = ts;
         op = ops;
+    }
+    static uint extractBits(uint word, int hi, int lo)
+    {
+        int width = lo - hi + 1;
+        uint mask = (1u << width) - 1;
+        return (word >> lo) & mask;
+    }
+
+    public void process_it(byte[] data)
+    {
+        uint word = BitConverter.ToUInt32(data, 0);
     }
 }
