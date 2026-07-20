a4534e825bed33c3c52382c8de03a1c2129eb6de
mohamed amdouni <midouamdouni4@gmail.com>
2026-07-20 20:10:46 +0000

    DP_scalar bucket initiated

--- ByteCarve/Services/DP_scalar.cs
+++ ByteCarve/Services/DP_scalar.cs
@@ -2,5 +2,11 @@ namespace ByteCarve.Services;
 
 public class DP_scalar
 {
-    
+    private string op;
+    ulong index;
+    public DP_scalar(ulong ts,string ops)
+    {
+        index = ts;
+        op = ops;
+    }
 }
